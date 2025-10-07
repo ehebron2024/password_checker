ce2c23ccc8b95e741c4fb8a994f80cb3a90961b6
ehebron2024 <ehebron2024@fau.edu>
2025-10-07 14:05:45 -0400

    Create password.py

---
+++ password.py
@@ -0,0 +1,26 @@
+import string 
+import re
+import getpass
+
+# Minimum length of pass
+MIN_LENGTH = 10
+
+def get_password():
+    print("Welcome to Eden's secure password checker.")
+    print("Enter 'quit' to quit")
+    
+    pass_input = input("Please enter a password: ")
+    return pass_input
+
+def check_length(pass_input)
+
+
+
+def main():
+    pass_input = get_password()
+    check_length(pass_input)
+
+
+
+if __name__ == "__main__":
+    main()
